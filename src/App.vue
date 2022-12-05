e15603744d200eb8a20d42e8cf1d1f3361ca23aa
LionelSebastian <lionel.sebastian.cifo@gmail.com>
2022-12-05 19:23:25 +0100

    FIX => Styling with Tailwind..just missing wrap of the main section

--- src/App.vue
+++ src/App.vue
@@ -14,35 +14,37 @@
 
 <template>
   <div id="app">
-    <nav-bar class="flex justify-between ml-2 mr-2 mt-2 mb-2">
+
+    <nav-bar class="flex justify-between ml-7 mr-7 mt-7">
       <img :src="logoIron" alt="Logo IronHack">
       <img src="../src/images/Burger_menuvue-lab.png" alt="Burger menu Logo">
     </nav-bar>
 
-    <header class="flex flex-col ">
-      <img :src="logoVue" alt="Logo Vue" class="self-center pt-10 pb-10">
+    <header>
+      <img :src="logoVue" alt="Logo Vue" class=" mx-auto pt-10 pb-3">
       <h1 class="text-center text-5xl pb-5 pt-5 font-bold">Welcome to Vue.js</h1>
-      <h4 class=" text-center self-center max-w-[30%] pb-6">Become a coding ninja with the fastest-growing JavaSript framework</h4>
-      <button>Awesome</button>
+      <h4 class="mx-auto text-center max-w-[40%] font-bold pb-6">Become a coding ninja with the fastest-growing JavaSript framework</h4>
+      <button class=" flex mx-auto rounded-full bg-[#34495E] p-1 pl-5 pr-5 mb-3 color: text-white">Awesome</button>
     </header>
 
     <main class="flex justify-around flex-wrap">
       <div :class="containers">
-        <img :src="img1" alt="Performant Logo" class=" place-self-center justify-self-center object-center self-center ">
-        <h4 class="text-3xl pt-1 text-center font-medium">Performant</h4>
+        <img :src="img1" alt="Performant Logo" class=" mx-auto ">
+        <h4 class="text-2xl text-center font-medium pt-4 pb-3">Performant</h4>
         <p class="text-2xs pt-1 text-center">Truly ractive, compiler-optimized, y ya no quiero copiar mas, voy a escribir nomas</p>
       </div>
       <div :class="containers">
-        <img src="../src/images/USP-2vue-lab.png" alt="Approachable logo">
-        <h4 class="text-3xl text-center font-medium pt-1">Approachable</h4>
-        <p class="text-2xs pt-1 text-center">Build on top pf standar HTML, CSS and JavaScript with intituive API and world class documentacition</p>
+        <img src="../src/images/USP-2vue-lab.png" alt="Approachable logo" class=" mx-auto">
+        <h4 class="text-2xl text-center font-medium pt-4 pb-3">Approachable</h4>
+        <p class="text-3xs pt-1 text-center">Build on top pf standar HTML, CSS and JavaScript with intituive API and world class documentacition</p>
       </div>
       <div :class="containers">
-        <img src="../src/images/USP-3vue-lab.png" alt="Verstaile logo">
-        <h4 class="text-3xl pt-1 text-center font-medium">Versatile</h4>
+        <img src="../src/images/USP-3vue-lab.png" alt="Verstaile logo" class=" mx-auto">
+        <h4 class="text-2xl text-center font-medium pt-4 pb-3">Versatile</h4>
         <p class="text-2xs pt-1 text-center">A rich, incrementally adaptable ecosystem taht scales between a library and a full featured framework</p>
       </div>
     </main>
+
   </div>
 </template>
 
@@ -52,14 +54,8 @@
    padding: 7%;
     background-color:  #39B983;
    }
-   button{
-    margin: 2%;
-    padding:  1%;
-    background-color: aquamarine;
-    border-radius: 15%;
-   }
-   .cajita{
-    width: 30%;
+  .cajita{
+    width: 25%;
     align-items: center;
    }
 </style>
