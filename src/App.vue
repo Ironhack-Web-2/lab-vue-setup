7b33bb77aabf3bee9d1d5222413469d7a36b4f79
Marina M <marnacode@gmail.com>
2022-12-05 18:13:33 +0100

    replicate page style

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,55 @@
 </script>
 
 <template>
-
+        <div class="mt-5 min-w-[40rem] ">
+                <nav class="flex justify-between py-1 mx-10">
+                        <div>
+                                <img src="./assets/Ironhack_logo.png" />
+                        </div>
+                        <div>
+                                <img src="./assets/Burger_menu.png" />
+                        </div>
+                </nav>
+                <div class="flex flex-col items-center justify-center h-fit my-20">
+                        <div>
+                                <img src="./assets/Vue_logo.png" />
+                        </div>
+                        <h1 class="text-5xl font-extrabold text-baseblack font-inter">Welcome to Vue.js</h1>
+                        <h2 class="text-s font-inter font-extrabold text-center py-5 w-96">Becoming a conding ninja with
+                                the fastest-growing JavaScript framework</h2>
+                        <button
+                                class="bg-secondary text-white font-extrabold text-s font-inter p-2 px-10 rounded-2xl">Awesome!</button>
+                </div>
+                <div class="bg-primary h-[22rem] p-10 flex justify-around items-center text-center">
+                        <div class="flex flex-col items-center w-44">
+                                <div>
+                                        <img class="h-18" src="./assets/USP-1.png" />
+                                </div>
+                                <h3 class="font-bold font-inter py-5">Performant</h3>
+                                <p class="font-inria text-sm tracking-tight">Truly reactive, compiler-optimized
+                                        rendering system that rarely requires manual
+                                        optimization.</p>
+                        </div>
+                        <div class="flex flex-col items-center w-44">
+                                <div>
+                                        <img class="h-18" src="./assets/USP-2.png" />
+                                </div>
+                                <h3 class="font-bold font-inter py-5">Approachable</h3>
+                                <p class="font-inria text-sm tracking-tight">Builds on top of standard HTML, CSS and
+                                        JavaScript with intuitive API and
+                                        world-class documentation.</p>
+                        </div>
+                        <div class="flex flex-col items-center w-44">
+                                <div>
+                                        <img class="h-18" src="./assets/USP-3.png" />
+                                </div>
+                                <h3 class="font-bold font-inter py-5">Versatile</h3>
+                                <p class="font-inria text-sm tracking-tight">A rich, incrementally adoptable ecosystem
+                                        that scales between a library and a
+                                        full-featured framework.</p>
+                        </div>
+                </div>
+        </div>
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
         content: ["./index.html",
                 "./src/**/*.{vue,js,ts,jsx,tsx}",],
         theme: {
-                extend: {},
+                extend: {
+                        colors: {
+                                primary: '#39B983',
+                                secondary: '#34495E',
+                                baseblack: '#2B2F30'
+                        },
+                        height: {
+                                "10v": "10vh",
+                                "20v": "20vh",
+                                "30v": "30vh",
+                                "40v": "40vh",
+                                "50v": "50vh",
+                                "60v": "60vh",
+                                "70v": "70vh",
+                                "80v": "80vh",
+                                "90v": "90vh",
+                                "100v": "100vh",
+                        },
+                        fontFamily: {
+                                'inter': ['Inter', 'sans-serif'],
+                                'inria': ['Inria', 'sans-serif'],
+                        },
+                },
         },
         plugins: [],
 }
